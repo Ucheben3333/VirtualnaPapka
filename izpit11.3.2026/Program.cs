d19ef732fd0f3b763d3feaba87718cb57f2068ab
Ucheben3333 <uchebengithub@gmail.com>
2026-03-11 10:06:43 +0200

    now11.3.2026

---
+++ izpit11.3.2026/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace izpit11._3._2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+             .Split(' ')
+             .Select(int.Parse)
+             .ToArray();
+            Console.WriteLine(string.Join(" ", numbers));
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Nqma vkarani chisla");
+            }
+            string[] number = input.Split(' ');
+
+
+        }
+    }
+}
