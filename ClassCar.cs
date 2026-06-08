93c784dd922be7c5293a8e87810a71b80c49caa3
Ucheben3333 <uchebengithub@gmail.com>
2026-06-08 08:58:24 +0300

    Project Cars 3.6.2026

---
+++ ClassCar.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nov3._6._2026
+{
+    internal class ClassCar
+    {
+        private string marka;
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int godina;
+        public int Godina
+        {
+            get { return godina; }
+            set { if (value < 1980) 
+            {
+                value = 2000;
+            }
+            godina = value; 
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Marka: {Marka}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Godina: {Godina}");
+        }
+        public ClassCar(string marka, string model, int godina)
+        {
+            Marka = marka;
+            Model = model;
+            Godina = godina;
+            count++;
+        }
+        public void sustoqnie()
+        {
+            if (this.godina > 2020)
+            {
+                Console.WriteLine("Kolata e nova");
+            }
+            else
+            {
+                Console.WriteLine("Kolata e stara");
+            }
+
+        }
+        static private int count;
+        static public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+            }
+        }
+        public ClassCar()
+        {
+            Marka = "";
+            Model = "";
+            Godina = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace nov3._6._2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ClassCar> cars = new List<ClassCar>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i< n ; i++ )
+            {
+                Console.WriteLine("-------------------");
+                Console.Write("Marka: ");
+                string brand = Console.ReadLine();
+                Console.Write("Model: ");
+                string model = Console.ReadLine();
+                Console.Write("Godina: ");
+                int godina = int.Parse(Console.ReadLine());
+
+                ClassCar car = new ClassCar(brand, model, godina);
+                cars.Add(car);
+            }
+            foreach(ClassCar car in cars)
+            {
+                Console.WriteLine("-------------------");
+                car.PrintInfo();
+                car.sustoqnie();
+            }
+            Console.WriteLine($"Broi koli: {ClassCar.Count}");
+            Console.WriteLine("-------------------");
+        }
+    }
+}
